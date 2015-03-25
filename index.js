fc70637c8f5f4e3ad06b2c26455b1065dedf42cf
Matthew Wright <matthew@smallhelm.com>
2015-03-25 17:01:59 -0600

    adding justRun

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var fs = require('fs');
 var async = require('async');
 
-module.exports = function(config, cb){
+var build = function(config, cb){
 	async.each(config.pages, function(page, cb){
 		async.seq(page.render, function(data, cb){
 			fs.writeFile(config.public_folder + page.url, data, cb);
 		})(config, cb);
 	}, cb);
+};
+
+module.exports = build;
+module.exports.justRun = function(config){
+	var start = (new Date()).getTime();
+	console.log("building", config.domain);
+	build(config, function(err){
+		if(err){
+			console.error("Error building", config.domain, err);
+		}
+		console.log("built", config.domain, "in", ((new Date()).getTime() - start) + "ms");
+	});
 };
