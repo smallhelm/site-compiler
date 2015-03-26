3c3bc8b1dcf8ab4e20c88cca200f3d9edf05d7ae
Matthew Wright <matthew@smallhelm.com>
2015-03-26 03:05:02 -0600

    config.pages is now config.files

--- renderers/sitemap-xml.js
+++ renderers/sitemap-xml.js
@@ -2,7 +2,7 @@ module.exports = function(config, cb){
 	var root_url = "http://" + config.domain;
 	var sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	sitemap += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
-	sitemap += config.pages.filter(function(page){
+	sitemap += config.files.filter(function(page){
 		return !page.exclude_from_sitemap;
 	}).map(function(page){
 		return "  <url>\n    <loc>" + root_url + page.url.replace(/index.html$/, "") + "</loc>\n  </url>\n";
